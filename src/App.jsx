3e9874638d032a36baa82e24810fe361e5d1ff92
Said Khail <saidkhail@hotmail.com>
2024-03-09 16:17:57 +0000

    Added npm packages, components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 700,
+      easing: 'ease',
+      delay: 100,
+    })
+  })
+
   return (
     <>
-      <div className='bg-red-400'>App</div>
+      <div className='overflow-x-hidden'>
+        App
+        <Navbar />
+        <HomePage />
+        <AboutUs />
+        <Menu />
+        <ContactPage />
+      </div>
     </>
   )
 }
