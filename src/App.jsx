e9e85aa5e988fb6b59e9ee101a30f3f553437637
Said Khail <saidkhail@hotmail.com>
2024-03-27 15:42:10 +0000

    Fixed: Routes, Coffee Menu, and Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React, { useEffect } from 'react'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 //Importing Components
-import Home from './components/home/Homepage'
-import Navigation from './components/navigation/Navbar'
-import About from './components/about/About'
-import Shop from './components/shop/Shop'
-import Menu from './components/menu/CoffeeMenu'
-import Blog from './components/blog/Blog'
-import OurMenu from './components/ourmenu/OurMenu'
-import ContactPage from './components/contactpage/ContactPage'
+import Home from './components/home/Homepage';
+import Navigation from './components/navigation/Navbar';
+import About from './components/about/About';
+import Shop from './components/shop/Shop';
+import Menu from './components/menu/CoffeeMenu';
+import Blog from './components/blog/Blog';
+import OurMenu from './components/ourmenu/OurMenu';
 
 function App() {
   return (
@@ -19,18 +17,16 @@ function App() {
       <Router>
         <Navigation />
         <Routes>
-          <Route path='/' element={<Home />}>
-            <Route path='/about' element={<About />} />
-            <Route path='/shop' element={<Shop />} />
-
-            <Route path='/blog' element={<Blog />} />
-            <Route path='/ourmenu' element={<OurMenu />} />
-            <Route path='/menu' element={<Menu />} />
-          </Route>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/ourmenu' element={<OurMenu />} />
+          <Route path='/CoffeeMenu' element={<Menu />} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/menu/CoffeeMenu.jsx
+++ src/components/menu/CoffeeMenu.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import '../menu/CoffeeStyles.css';
 
 const CoffeeMenu = () => {
-  return <div>CoffeeMenu</div>
-}
-
-export default CoffeeMenu
+  return (
+    <>
+      <div className='menuBody'>
+        <div className=' flex justify-center items-center'>
+          <div className='text-center'>
+            <h1 className='font-rakkas uppercase text-customWhite tracking-widest'>MENU </h1>
+            <p className='leading-8 font-sans text-customWhite py-5'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure earum iste beatae suscipit necessitatibus ex sed eaque officia natus ducimus!
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default CoffeeMenu;
